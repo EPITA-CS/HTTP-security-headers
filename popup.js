c09c89fb5a0d5e9658c319c820833817b4ddc447
ShannonSD <shannonsaviodsouza@gmail.com>
2021-02-22 17:18:47 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,12 +2,12 @@ chrome.tabs.query({active: true, currentWindow: true},function(tab){
     var grabheader=chrome.extension.getBackgroundPage();
 recHeaders=grabheader.headers[tab[0].id];
 console.log(recHeaders);
-if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='content-security-policy'))
+if(recHeaders.responseHeaders.find((a=>a.name.toLowerCase()==='content-security-policy')&&(a=>a.value.includes('default-src none'))))
 { 
- if(recHeaders.responseHeaders.find(a=>a.value.toLowerCase()==='default-src'))
-       {main.innerHTML+="<td> Not a strong CSP </td>"
-           if(recHeaders.responseHeaders.find(a=>a.value.toLowerCase()==='default-src *'))
-           main.innerHTML+="<td> Not a strong CSP </td>";}
+    main.innerHTML+="<tr><td>Content-Security-Policy</td><td> insecure </td></tr>"
+    //if((a=>a.name.toLowerCase()==='content-security-policy')&&(a=>a.value.includes('default-src self')))
+    //main.innerHTML+="<tr><td>Content-Security-Policy</td><td> star </td></tr>"
+//document.getElementById("headerid").innerHTML+="CSP exists";
 }
 else
 main.innerHTML+="<td> Content-Security-Policy missing </td>"
@@ -16,22 +16,6 @@ if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='strict-transport-s
 main.innerHTML+="<td> HSTS exists </td>"
 //document.getElementById("headerid").innerHTML+="HSTS exists";
 else
-main.innerHTML+="<td> HSTS doesn't exists </td>"
+main.innerHTML+="<td> HSTS exists </td>"
 //document.getElementById("headerid").innerHTML+="HSTS missing";
-if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='x-frame-options'))
-{
- if(recHeaders.responseHeaders.find(a=>a.value.toUpperCase()==='DENY'))
- main.innerHTML+="<td> x-frame-options is strong </td>"
-}
-else
-main.innerHTML+="<td> x-frame-options doesn't exist </td>"
-if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='x-xss-protection'))
-main.innerHTML+="<td> x-xss-protection exists </td>"
-else
-main.innerHTML+="<td> x-xss-protection doesn't exist </td>"
-
-
-
-
-
 });
