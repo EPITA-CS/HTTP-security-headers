083a593ab7da2e16c613893ec00caf86290429a1
ShannonSD <shannonsaviodsouza@gmail.com>
2021-02-22 14:35:27 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,10 +3,11 @@ chrome.tabs.query({active: true, currentWindow: true},function(tab){
 recHeaders=grabheader.headers[tab[0].id];
 console.log(recHeaders);
 if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='content-security-policy'))
-{
 document.getElementById("headerid").innerHTML+="CSP exists";
-}
 else
 document.getElementById("headerid").innerHTML+="CSP missing";
-
+if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='strict-transport-security'))
+document.getElementById("headerid").innerHTML+="HSTS exists";
+else
+document.getElementById("headerid").innerHTML+="HSTS missing";
 });
