08d4d9c31a8b13c82aae13f5b0935eb7a0e380fd
Balanagameena <65198900+Balanagameena@users.noreply.github.com>
2021-02-22 16:00:19 +0100

    Add files via upload

--- popup.js
+++ popup.js
@@ -3,15 +3,18 @@ chrome.tabs.query({active: true, currentWindow: true},function(tab){
 recHeaders=grabheader.headers[tab[0].id];
 console.log(recHeaders);
 if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='content-security-policy'))
-main.innerHTML+="<td> Content-Security-Policy exists </td>"
-//document.getElementById("headerid").innerHTML+="CSP exists";
+{ main.innerHTML+="<td> Content-Security-Policy exists </td>"
+    if(recHeaders.responseHeaders.find(a=>a.value.toLowerCase()==='default-src'))
+       {if(recHeaders.responseHeaders.find(a=>a.value.toLowerCase()==='default-src *'))
+        main.innerHTML+="<td> Not a strong CSP </td>";}
+}
 else
-main.innerHTML+="<td> Content-Security-Policy missing </td>"
+    main.innerHTML+="<td> Content-Security-Policy missing </td>"
 //document.getElementById("headerid").innerHTML+="CSP missing";
 if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='strict-transport-security'))
 main.innerHTML+="<td> HSTS exists </td>"
 //document.getElementById("headerid").innerHTML+="HSTS exists";
 else
-main.innerHTML+="<td> HSTS exists </td>"
+main.innerHTML+="<td> HSTS doesn't exists </td>"
 //document.getElementById("headerid").innerHTML+="HSTS missing";
 });
