4738d9a7a7c6df8bfc1d4fc96ac3dca8004ff76f
Lokesh0809 <64198711+Lokesh0809@users.noreply.github.com>
2021-02-22 18:32:49 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -8,4 +8,14 @@ if(recHeaders.responseHeaders.find((a=>a.name.toLowerCase()==='content-security-
     //if((a=>a.name.toLowerCase()==='content-security-policy')&&(a=>a.value.includes('default-src self')))
     //main.innerHTML+="<tr><td>Content-Security-Policy</td><td> star </td></tr>"
 //document.getElementById("headerid").innerHTML+="CSP exists";
-
+}
+else
+main.innerHTML+="<td> Content-Security-Policy missing </td>"
+//document.getElementById("headerid").innerHTML+="CSP missing";
+if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='strict-transport-security'))
+main.innerHTML+="<td> HSTS exists </td>"
+//document.getElementById("headerid").innerHTML+="HSTS exists";
+else
+main.innerHTML+="<td> HSTS exists </td>"
+//document.getElementById("headerid").innerHTML+="HSTS missing";
+});
