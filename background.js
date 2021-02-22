ee6f43ac75180a2dc52e50350320f40f0a0e188e
ShannonSD <shannonsaviodsouza@gmail.com>
2021-02-22 12:07:35 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -1,29 +1,11 @@
+var headers={};
+var secureheaders={};
 
-/* var currentSettings; */
+chrome.webRequest.onHeadersReceived.addListener(function(details){
+headers[details.tabId]=details;
+},{urls: ["<all_urls>"],types: ["main_frame"]},["responseHeaders"]);
 
-var headers = {};
-
-var filters = {
-  urls: ["<all_urls>"],
-  /* types: ["main_frame"] */
-};
-
-/* headers received */
-chrome.webRequest.onHeadersReceived.addListener(function(details) {
-headers[details.tabId] = headers[details.tabId] || {};
-headers[details.tabId] = details;
-}, filters, ["responseHeaders"]);
-
-/* remove tab data from headers object when tab is onRemoved */
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-	delete headers[tabId];
+chrome.tabs.onRemoved.addListener(function(tabId,removeInfo)
+{
+delete headers[tabId];
 });
-
-/*function get_options() {
-  chrome.storage.sync.get(
- 		function (settings) {
- 			currentSettings = settings;
- 		}
- 	);
-}
-get_options();*/
