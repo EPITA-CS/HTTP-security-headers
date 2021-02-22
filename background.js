a85ab7f091bd99857de704e3c1cb39d0495af4dd
Balanagameena <65198900+Balanagameena@users.noreply.github.com>
2021-02-22 11:31:22 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -1,11 +1,29 @@
-var headers={};
-var secureheaders={};
 
-chrome.webRequest.onHeadersReceived.addListener(function(details){
-headers[details.tabId]=details;
-},{urls: ["<all_urls>"],types: ["main_frame"]},["responseHeaders"]);
+/* var currentSettings; */
 
-chrome.tabs.onRemoved.addListener(function(tabId,removeInfo)
-{
-delete headers[tabId];
+var headers = {};
+
+var filters = {
+  urls: ["<all_urls>"],
+  /* types: ["main_frame"] */
+};
+
+/* headers received */
+chrome.webRequest.onHeadersReceived.addListener(function(details) {
+headers[details.tabId] = headers[details.tabId] || {};
+headers[details.tabId] = details;
+}, filters, ["responseHeaders"]);
+
+/* remove tab data from headers object when tab is onRemoved */
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+	delete headers[tabId];
 });
+
+/*function get_options() {
+  chrome.storage.sync.get(
+ 		function (settings) {
+ 			currentSettings = settings;
+ 		}
+ 	);
+}
+get_options();*/
