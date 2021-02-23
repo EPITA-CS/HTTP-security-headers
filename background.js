5fbe0954670d7af63f3c8d1a28f8d432b3e90fcd
Balanagameena <65198900+Balanagameena@users.noreply.github.com>
2021-02-23 12:29:03 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 var headers={};
 var secureheaders={};
-
 chrome.webRequest.onHeadersReceived.addListener(function(details){
 headers[details.tabId]=details;
 console.log(details);
@@ -8,21 +7,31 @@ if(headers[details.tabId].responseHeaders.find(a=>a.name.toLowerCase()==='conten
 {
 if(headers[details.tabId].responseHeaders.find((a=>a.name.toLowerCase()==='content-security-policy')&&(a=>a.value.includes('default-src none')))) //this check works
 { 
-    secureheaders[details.tabId]+="<tr><td>Content-Security-Policy:</td><td> default-src none </td></tr>"
-}
-if(headers[details.tabId].responseHeaders.find((a=>a.name.toLowerCase()==='content-security-policy')&&(a=>a.value.includes('default-src *'))))
+secureheaders[details.tabId]+="<tr class=\"strong\"><td>Content-Security-Policy:</td><td>default-src 'none'</td></tr>"}
+else if(headers[details.tabId].responseHeaders.find((a=>a.name.toLowerCase()==='content-security-policy')&&(a=>a.value.includes('default-src *'))))
 {
-    secureheaders[details.tabId]+="<tr><td>Content-Security-Policy:</td><td> default-src * </td></tr>"
+secureheaders[details.tabId]+="<tr class=\"weak\"><td>Content-Security-Policy:</td><td> default-src * </td></tr>"
 }
-if(headers[details.tabId].responseHeaders.find((a=>a.name.toLowerCase()==='content-security-policy')&&(a=>a.value.includes("default-src 'self'"))))
+else if(headers[details.tabId].responseHeaders.find((a=>a.name.toLowerCase()==='content-security-policy')&&(a=>a.value.includes("default-src 'self'"))))
 {
-    secureheaders[details.tabId]+="<tr><td>Content-Security-Policy:</td><td> default-src self </td></tr>"
+secureheaders[details.tabId]+="<tr class=\"strong\"><td>Content-Security-Policy:</td><td> default-src 'self' </td></tr>"
 }
 }
 else
-secureheaders[details.tabId]+="<td> Content-Security-Policy </td><td>missing </td>";
-console.log(secureheaders);
-},{urls: ["<all_urls>"],types: ["main_frame"]},["responseHeaders"]);
+secureheaders[details.tabId]+="<td> Content-Security-Policy </td><td>missing</td>";
+if(headers[details.tabId].responseHeaders.find((a=>a.name.toLowerCase()==='strict-transport-security')))
+ {if(headers[details.tabId].responseHeaders.find((a=>a.name.toLowerCase()==='strict-transport-security')&&(a=>a.value.includes("max-age=0"))))
+ secureheaders[details.tabId]+="<tr class=\"weak\"><td> strict-transport-security </td><td> max-age is zero </td>"; 
+ else {
+ secureheaders[details.tabId]+="<tr class=\"strong\"><td> strict-transport-security </td><td> max-age not zero</td>";} 
+ if (headers[details.tabId].responseHeaders.find((a=>a.name.toLowerCase()==='strict-transport-security')&&(a=>a.value.includes("includeSubDomains"))))
+ secureheaders[details.tabId]+="<tr class=\"strong\"><td>strict-transport-security </td><td> includeSubdomains </td>"; 
+ }
+else
+secureheaders[details.tabId]+="<td> strict-transport-security </td><td>missing</td>";
+
+
+},{urls: ["<all_urls>"],types:["main_frame"]},["responseHeaders"]);
 
 chrome.tabs.onRemoved.addListener(function(tabId,removeInfo)
 {
