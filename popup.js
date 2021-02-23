7e05283a87851f0d0ff841094ce513807949236d
Balanagameena <65198900+Balanagameena@users.noreply.github.com>
2021-02-23 09:20:45 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,14 +1,10 @@
 chrome.tabs.query({active: true, currentWindow: true},function(tab){
-    var grabheader=chrome.extension.getBackgroundPage();
+var grabheader=chrome.extension.getBackgroundPage();
 recHeaders=grabheader.headers[tab[0].id];
 console.log(recHeaders);
-if(recHeaders.responseHeaders.find((a=>a.name.toLowerCase()==='content-security-policy')&&(a=>a.value.includes('default-src none'))))
-{ 
-    main.innerHTML+="<tr><td>Content-Security-Policy</td><td> insecure </td></tr>"
-    //if((a=>a.name.toLowerCase()==='content-security-policy')&&(a=>a.value.includes('default-src self')))
-    //main.innerHTML+="<tr><td>Content-Security-Policy</td><td> star </td></tr>"
-//document.getElementById("headerid").innerHTML+="CSP exists";
-}
+if(recHeaders.responseHeaders.find((a=>a.name.toLowerCase()==='content-security-policy')&&(a=>a.value.includes('default-src *'))))
+       {
+           main.innerHTML+="<td>Content security policy is insecure</td>";}
 else
 main.innerHTML+="<td> Content-Security-Policy missing </td>"
 //document.getElementById("headerid").innerHTML+="CSP missing";
@@ -16,6 +12,27 @@ if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='strict-transport-s
 main.innerHTML+="<td> HSTS exists </td>"
 //document.getElementById("headerid").innerHTML+="HSTS exists";
 else
-main.innerHTML+="<td> HSTS exists </td>"
+main.innerHTML+="<td> HSTS doesn't exists </td>"
 //document.getElementById("headerid").innerHTML+="HSTS missing";
+if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='x-frame-options'))
+{
+ if(recHeaders.responseHeaders.find(a=>a.value.toUpperCase()==='DENY'))
+ main.innerHTML+="<td> x-frame-options is strong </td>"
+}
+else
+main.innerHTML+="<td> x-frame-options doesn't exist </td>"
+if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='x-xss-protection'))
+{
+    if(recHeaders.responseHeaders.find(a=>a.value ==='1'))
+    main.innerHTML+="<td> x-xss-protection is strong </td>"
+}
+else
+main.innerHTML+="<td> x-xss-protection doesn't exist </td>"
+if(recHeaders.responseHeaders.find(a=>a.name.toLowerCase()==='x-content-type-options'))
+{
+   if(recHeaders.responseHeaders.find(a=>a.value.toLowerCase()==='nosniff'))
+   main.innerHTML+="<td> x-content-type-options is strong</td>"
+}
+else
+main.innerHTML+="<td> x-content-type-options doesn't exist </td>"
 });
