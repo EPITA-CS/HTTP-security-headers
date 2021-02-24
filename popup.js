e4eeba66c244e405abe73cb9849f98dd8dd7ace0
ShannonSD <shannonsaviodsouza@gmail.com>
2021-02-24 11:09:14 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,16 @@
 chrome.tabs.query({active: true, currentWindow: true},function(tab){
     var grabheader=chrome.extension.getBackgroundPage();
-recHeaders=grabheader.secureheaders[tab[0].id];
+    console.log(grabheader.secureheaders[tab[0].id]);
+    if(typeof grabheader.secureheaders[tab[0].id] == "undefined")
+    errorbody.innerHTML= "Header not captured, please refresh the page or clear cache with CTRL+F5.";
+else{
+    main.innerHTML="<tr><th>Header</th><th>Value</th><th>Config</tr>";
+    recHeaders=grabheader.secureheaders[tab[0].id];
+if(recHeaders.includes("undefined"))
+recHeaders=recHeaders.replace("undefined", "");
+logs=grabheader.headers[tab[0].id];
+console.log(logs);
 console.log(recHeaders);
-main.innerHTML=recHeaders;
+main.innerHTML+=recHeaders;
+}
 });
