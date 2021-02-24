8ef43c2cb254c6d34dc453ab987faaedc7fa4ca1
Balanagameena <65198900+Balanagameena@users.noreply.github.com>
2021-02-24 11:48:08 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -5,28 +5,29 @@ chrome.webRequest.onHeadersReceived.addListener(function(details){
 console.log(details)
 headers[details.tabId]=details.responseHeaders;
 headers[details.tabId].csp=headers[details.tabId].hsts=headers[details.tabId].xss=headers[details.tabId].xfo=headers[details.tabId].xct=headers[details.tabId].rp=0;
+
 for(i=0;i<headers[details.tabId].length;i++)
 {
     if(headers[details.tabId][i].name==="content-security-policy") //all conditions aren't checked this is a sample
     {
         headers[details.tabId].csp=1;
         if(headers[details.tabId][i].value.includes("default-src 'none'"))
-        secureheaders[details.tabId]+="<tr class=\"strong\"><td>Content-Security-Policy:</td><td> default-src none </td></tr>"
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td>Content-Security-Policy:</td><td> default-src none </td><td><i class=\"fa fa-check\"></i></td></tr>"
         if(headers[details.tabId][i].value.includes("default-src 'self'"))
-        secureheaders[details.tabId]+="<tr class=\"strong\"><td>Content-Security-Policy:</td><td> default-src self </td></tr>"
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td>Content-Security-Policy:</td><td> default-src self </td><td> <i class=\"fa fa-check\"></i></td></tr>"
         if(headers[details.tabId][i].value.includes("default-src *"))
-        secureheaders[details.tabId]+="<tr class=\"weak\"><td>Content-Security-Policy:</td><td> default-src * </td></tr>"
+        secureheaders[details.tabId]+="<tr class=\"weak\"><td>Content-Security-Policy:</td><td> default-src * </td> <td> <i class=\"fa fa-exclamation\"></i></td></tr>"
         if(!headers[details.tabId][i].value.includes("default-src"))
-        secureheaders[details.tabId]+="<tr class=\"weak\"><td>Content-Security-Policy:</td><td> default-src missing </td></tr>";
+        secureheaders[details.tabId]+="<tr class=\"weak\"><td>Content-Security-Policy:</td><td> default-src missing </td><td> <i class=\"fa fa-exclamation\"></i></td></tr>";
     }  
 
     if(headers[details.tabId][i].name==="strict-transport-security")
     {
         headers[details.tabId].hsts=1;
         if(headers[details.tabId][i].value.includes("max-age=0"))
-        secureheaders[details.tabId]+="<tr class=\"weak\"><td>Strict-Transport-Security:</td><td> max-age=0 </td></tr>"
+        secureheaders[details.tabId]+="<tr class=\"weak\"><td>Strict-Transport-Security:</td><td> max-age=0 </td><td> <i class=\"fa fa-exclamation\"></i></td></tr>"
         if(!headers[details.tabId][i].value.includes("max-age"))
-        secureheaders[details.tabId]+="<tr class=\"weak\"><td>Strict-Transport-Security:</td><td> max-age missing </td></tr>"
+        secureheaders[details.tabId]+="<tr class=\"weak\"><td>Strict-Transport-Security:</td><td> max-age missing </td><td> <i class=\"fa fa-exclamation\"></i></td></tr>"
         else{
             var newstr=headers[details.tabId][i].value.substring('max-age='.length);
             //newstr=newstr.substring('max-age='.length);
@@ -34,23 +35,23 @@ for(i=0;i<headers[details.tabId].length;i++)
             secureheaders[details.tabId]+="<tr class=\"strong\"><td>Strict-Transport-Security:</td><td> max-age is "+num+"</td></tr>";
         }
         if(headers[details.tabId][i].value.includes("includeSubDomains"))
-        secureheaders[details.tabId]+="<tr class=\"strong\"><td>Strict-Transport-Security:</td><td> includeSubDomains </td></tr>"
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td>Strict-Transport-Security:</td><td> includeSubDomains </td><td><i class=\"fa fa-check\"></i></td></tr>"
         else
-        secureheaders[details.tabId]+="<tr class=\"weak\"><td>Strict-Transport-Security:</td><td> Missing includeSubDomains </td></tr>";
+        secureheaders[details.tabId]+="<tr class=\"weak\"><td>Strict-Transport-Security:</td><td> Missing includeSubDomains </td><td> <i class=\"fa fa-exclamation\"></i></td></tr>";
     }
 
     if(headers[details.tabId][i].name==="x-xss-protection")
     {
         headers[details.tabId].xss=1;
         if(headers[details.tabId][i].value.includes("0"))
-        secureheaders[details.tabId]+="<tr class=\"weak\"><td> x-xss-protection </td><td> zero </td></tr>";
+        secureheaders[details.tabId]+="<tr class=\"weak\"><td> x-xss-protection </td><td> zero </td><td> <i class=\"fa fa-exclamation\"></i></td></tr>";
         if(headers[details.tabId][i].value.includes("1; mode=block"))
-        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-xss-protection </td><td> one and mode is block </td></tr>";
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-xss-protection </td><td> one and mode is block </td><td><i class=\"fa fa-check\"></i></td></tr>";
         if(headers[details.tabId][i].value.includes("1; report=".urls))// fill this to get http/https 
 
-        secureheaders[details.tabId]+="<tr class=\"weak\"><td> x-xss-protection </td><td> URI is HTTP </td></tr>";
+        secureheaders[details.tabId]+="<tr class=\"weak\"><td> x-xss-protection </td><td> URI is HTTP </td><td><i class=\"fa fa-exclamation\"></i></td></tr>";
         else
-        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-xss-protection </td><td> URI is HTTPs </td></tr>";
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-xss-protection </td><td> URI is HTTPs </td><td><i class=\"fa fa-check\"></i></td></tr>";
 
     }
 
@@ -58,40 +59,42 @@ for(i=0;i<headers[details.tabId].length;i++)
     {
         headers[details.tabId].xfo=1;
         if(headers[details.tabId][i].value.includes("allow-from"))
-        secureheaders[details.tabId]+="<tr class=\"weak\"><td> x-frame-options </td><td> allow-from </td></tr>";
+        secureheaders[details.tabId]+="<tr class=\"weak\"><td> x-frame-options </td><td> allow-from </td><td> <i class=\"fa fa-exclamation\"></i></td></tr>";
         if(headers[details.tabId][i].value.includes("sameorigin"))
-        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-frame-options </td><td> sameorigin </td></tr>";
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-frame-options </td><td> sameorigin </td><td><i class=\"fa fa-check\"></i></td></tr>";
         if(headers[details.tabId][i].value.includes("deny"))
-        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-frame-options </td><td> deny </td></tr>";
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-frame-options </td><td> deny </td><td><i class=\"fa fa-check\"></i></td></tr>";
     }
 
     if(headers[details.tabId][i].name==="x-content-type-options")
     {
         headers[details.tabId].xct=1;
         if(headers[details.tabId][i].value.includes("nosniff"))
-        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-content-type-options </td><td> nosniff </td></tr>";
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-content-type-options </td><td> nosniff </td><td><i class=\"fa fa-check\"></i> </td></tr>";
         else
-        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-content-type-options </td><td> not nosniff </td></tr>";
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> x-content-type-options </td><td> not nosniff </td><td><i class=\"fa fa-check\"></i></td></tr>";
     }
 
     if(headers[details.tabId][i].name==="referrer-policy")
     {
         headers[details.tabId].rp=1;
         if(headers[details.tabId][i].value.includes("origin-when-cross-origin"))
-        secureheaders[details.tabId]+="<tr class=\"weak\"><td> referrer-policy </td><td> origin-when-cross-origin </td></tr>";
-        if(headers[details.tabId][i].value.includes("unsafe-url"))
-        secureheaders[details.tabId]+="<tr class=\"weak\"><td> referrer-policy</td><td>unsafe-url</td></tr>";
-        if(headers[details.tabId][i].value.includes("no-referrer"))
-        secureheaders[details.tabId]+="<tr class=\"strong\"><td> referrer-policy</td><td>no-referrer</td></tr>";
-        if(headers[details.tabId][i].value.includes("no-referrer-when-downgrade"))
-        secureheaders[details.tabId]+="<tr class=\"strong\"><td> referrer-policy</td><td>no-referrer-when-downgrade</td></tr>";
-        if(headers[details.tabId][i].value.includes("origin"))
-        ecureheaders[details.tabId]+="<tr class=\"strong\"><td> referrer-policy</td><td>origin</td></tr>";
-
+        secureheaders[details.tabId]+="<tr class=\"weak\"><td> referrer-policy </td><td> origin-when-cross-origin </td><td> <i class=\"fa fa-exclamation\"></i></td></tr>";
+        else if(headers[details.tabId][i].value.includes("unsafe-url"))
+        secureheaders[details.tabId]+="<tr class=\"weak\"><td> referrer-policy</td><td>unsafe-url</td><td> <i class=\"fa fa-exclamation\"></i></td></tr>";
+        else if(headers[details.tabId][i].value.includes("no-referrer"))
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> referrer-policy</td><td>no-referrer</td><td><i class=\"fa fa-check\"></i></td></tr>";
+        else if(headers[details.tabId][i].value.includes("no-referrer-when-downgrade"))
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> referrer-policy</td><td>no-referrer-when-downgrade</td><td><i class=\"fa fa-check\"></i></td></tr>";
+        else if(headers[details.tabId][i].value.includes("origin"))
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> referrer-policy</td><td>origin</td><td><i class=\"fa fa-check\"></i></td></tr>";
+        else if(headers[details.tabId][i].value.includes("same-origin"))
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> referrer-policy</td><td>same-origin</td><td><i class=\"fa fa-check\"></i></td></tr>";
+        else if(headers[details.tabId][i].value.includes("strict-origin"))
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> referrer-policy</td><td>strict-origin</td><td><i class=\"fa fa-check\"></i></td></tr>";
+        else if(headers[details.tabId][i].value.includes("strict-origin-when-cross-origin"))
+        secureheaders[details.tabId]+="<tr class=\"strong\"><td> referrer-policy</td><td>strict-origin-when-cross-origin</td><td><i class=\"fa fa-check\"></i></td></tr>";
     }
-
-
-
 }
 if(headers[details.tabId].csp===0)
 secureheaders[details.tabId]+="<tr class=\"weak\"><td>Content-Security-Policy:</td><td> non-existent </td></tr>";
